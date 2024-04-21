e6f52db472f709a22a8a7751db852a3464443479
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-04-22 01:48:54 +0400

    Add constants, use defaultdict for pipeline

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,23 +1,22 @@
 import datetime as dt
+from collections import defaultdict
 
-from pep_parse.settings import BASE_DIR
+from pep_parse.settings import BASE_DIR, RESULTS_DIR
 
 
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.statuses = {}
-        self.now = dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        self.statuses = defaultdict(int)
 
     def process_item(self, item, spider):
-        self.statuses[
-            item['status']
-        ] = self.statuses.get(item['status'], 0) + 1
+        self.statuses[item['status']] += 1
         return item
 
     def close_spider(self, spider):
         with open(
-            f'{BASE_DIR}/results/status_summary_{self.now}.csv',
+            f'{BASE_DIR}/{RESULTS_DIR}/status_summary_'
+            f'{dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.csv',
             mode='w',
             encoding='utf-8'
         ) as f:
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -2,19 +2,20 @@ from pathlib import Path
 
 BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ['pep_parse.spiders']
-NEWSPIDER_MODULE = 'pep_parse.spiders'
+NEWSPIDER_MODULE = f'{BOT_NAME}.spiders'
+SPIDER_MODULES = [NEWSPIDER_MODULE]
 
 ROBOTSTXT_OBEY = True
 
 BASE_DIR = Path(__file__).parent / '..'
+RESULTS_DIR = 'results'
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{RESULTS_DIR}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
